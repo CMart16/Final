e666d46a90fcf652399be2ba34d7754afb2e66fb
CMart16 <contactcorbin@gmail.com>
2023-12-12 15:24:54 -0500

    Go

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 import random
+import sqlite3 as sql
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('main.html')
-
-
-
-
+    pictures = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg']
+    random_number = random.randint(0, len(pictures)-1)
+    picture = pictures[random_number]
+    return render_template('main.html', picture=picture)
 
 @app.route('/gallery')
 def gallery():
